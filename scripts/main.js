98cb0f535ebcdc406c25f691ef069cf6b95fc0da
Kirill Sezen <kirill_sezen@mail.ru>
2026-06-28 02:12:01 +0300

    add micromodal and timezone edit button

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,50 @@
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
+MicroModal.init({
+  disableScroll: true,
+  openClass: 'is-open',
+});
+
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const cityLabel = document.getElementById('city-label');
+const selectEl = document.getElementById('timezone-select');
+const applyBtn = document.getElementById('apply-btn');
+
+let currentTimezone = 'Europe/Moscow';
+
 function updateTime() {
-    const now = dayjs();
+    const now = dayjs().tz(currentTimezone);
     
     const timeString = now.format('HH:mm:ss');
     const dateString = now.format('dddd, D MMMM, YYYY');
-    const city = String(dayjs.tz.guess());
-		const formattedCity = city.slice(0, city.indexOf('/')) + ' / ' + city.slice(city.indexOf('/') + 1);
+		const formattedCity = currentTimezone.replace('/', ' / ');
 
-    document.getElementById('time').textContent = timeString;
-    document.getElementById('date').textContent = dateString;
-		document.getElementById('city-label').textContent = formattedCity;
+    timeEl.textContent = timeString;
+    dateEl.textContent = dateString;
+    cityLabel.textContent = formattedCity;
 }
+
+function changeTimezone (newTimezone) {
+  currentTimezone = newTimezone;
+  selectEl.value = newTimezone;
+
+  updateTime();
+
+  MicroModal.close('modal-1');
+}
+
+ applyBtn.addEventListener('click', function() {
+  const selectedValue = selectEl.value;
+
+   if (selectedValue) {
+    changeTimezone(selectedValue);
+  } else {
+    alert('Пожалуйста, выберите часовой пояс');
+  }
+})
 
 updateTime();
 
 setInterval(updateTime, 1000);
-
